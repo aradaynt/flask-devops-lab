c0f3315213682c33b4e88c7fcc8012b4c818bf3f
Arad Ayntabli <arad.ayntabli@ontariotechu.net>
2025-11-16 13:23:14 -0500

    Adding Flake8 lint checking yml file

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello():
     return 'Hello, DevOps World!'
+
 
 if __name__ == '__main__':
     app.run()
